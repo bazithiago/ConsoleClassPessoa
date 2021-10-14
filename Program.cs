eca0623019cf7555e1d25fb0cb792d9018e5839a
bazithiago <thiagovieira.dev@gmail.com>
2021-10-14 19:39:55 -0300

    secont commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleClassPessoa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! ");
         }
     }
 }
