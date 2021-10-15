a79034d79e3f1b93b616b22688bb58f6f7378619
bazithiago <thiagovieira.dev@gmail.com>
2021-10-14 21:08:03 -0300

    Class pessoa finalizado

--- Pessoa.cs
+++ Pessoa.cs
@@ -1,13 +1,26 @@
 ﻿using System;
-
 namespace ConsoleClassPessoa
 {
     public class Pessoa
     {
-        public Pessoa() {
-            public string nome;
-            public int idade;
-            public string rg;
+        public string nome;
+        public int idade;
+        public string rg;
+
+        public void Acenar(Pessoa pessoa)
+        {
+            Console.WriteLine($"{this.nome} acenou para {pessoa.nome}");
+        }
+
+        public void PerguntaIdadePara(Pessoa pessoa) 
+        {
+            Console.WriteLine($"Olá, {pessoa.nome}, qual sua idade?");
+            pessoa.Idade();
+        }
+        public void Idade()
+        {
+            Console.WriteLine($"Eu, {this.nome}, tenho {this.idade}");
         }
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace ConsoleClassPessoa
             pessoa.nome = "Thiago";
             pessoa.idade = 32;
 
+            Pessoa pessoaSilvia = new Pessoa();
+            pessoaSilvia.nome = "Silvia";
+            pessoaSilvia.idade = 23;
+
+            pessoa.Acenar(pessoaSilvia);
+            pessoaSilvia.Idade();
+            pessoa.Idade();
+            pessoa.PerguntaIdadePara(pessoaSilvia);
         }
     }
 }
